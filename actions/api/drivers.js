28f08f9b31ffc2115cd3d4d39873ce1875d266e3
Adam Makuchowski <makuchowski.adam@gmail.com>
2022-07-13 21:30:20 +0200

    init new endpoints

--- actions/api/drivers.js
+++ actions/api/drivers.js
@@ -4,13 +4,31 @@ module.exports = {
     createDriver(request, response) {
 
         const newDriver = new Driver({
-           // TODO create new driver
-        })
-        
-        newDriver.save().then(() => {
-            console.log('Driver has been successfully save in db')
+            // TODO create new driver
         })
 
-        response.send('Driver has been successfully created')
-    }
+        newDriver.save().then(() => {
+            console.log('Driver has been successfully save in db')
+            response.send('Driver has been successfully created')
+        }).catch((error) => {
+            response.send(`Error while create new driver. Error ${error}`)
+        })
+
+    },
+    getDriver(request, response) {
+
+        
+    },
+    getAllDrivers(request, response) {
+
+       
+    },
+    editDriver(request, response) {
+
+        
+    },
+    deleteDriver(request, response) {
+
+        
+    },
 }
